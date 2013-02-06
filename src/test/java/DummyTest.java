397cda0e64e13cd2aa139204dac38b36a9d40866
Chris Pitts <chris@thirstybear.co.uk>
2013-02-06 18:31:42 +0000

    Initial empty state for FizzBuzz kata in Java. Contains a dummy test to prove test framework is working.

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class DummyTest {
+    @Test
+    public void proveTestFramework() {
+        assertEquals(2, 5);
+    }
+}
