f17faebda8f4a77685b3c0ed14fc19f8ae964dd5
Chris Pitts <chris@thirstybear.co.uk>
2013-02-07 18:56:11 +0000

    Step 8 - major refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,9 +1,60 @@
 public class FizzBuzz {
+
+    private final IntToString stringProcessor;
+
+    public FizzBuzz() {
+        stringProcessor = new Default();
+        IntToString fizz = new Fizz();
+        fizz.addNext(new Buzz());
+        stringProcessor.addNext(fizz);
+    }
+
     public String forValue(int i) {
-        String result = "";
-        if (i % 3 == 0) result = "Fizz";
-        if (i % 5 == 0) result += "Buzz";
-        if (result.equals("")) result = String.valueOf(i);
-        return result;
+        return stringProcessor.addToString(i, "");
+    }
+
+    abstract class IntToString {
+        protected IntToString next = null;
+
+        public void addNext(IntToString processor) {
+            next = processor;
+        }
+
+        public abstract String addToString(int i, String valueSoFar);
+    }
+
+    class Fizz extends IntToString {
+        public String addToString(int i, String valueSoFar) {
+            if (i % 3 == 0) valueSoFar += "Fizz";
+            if (next != null) {
+                return next.addToString(i, valueSoFar);
+            }
+            return valueSoFar;
+        }
+    }
+
+    class Buzz extends IntToString {
+        public String addToString(int i, String valueSoFar) {
+            if (i % 5 == 0) valueSoFar += "Buzz";
+            if (next != null) {
+                return next.addToString(i, valueSoFar);
+            }
+            return valueSoFar;
+        }
+    }
+
+
+    class Default extends IntToString {
+        public String addToString(int i, String valueSoFar) {
+            String result = null;
+            if (next != null) {
+                result = next.addToString(i, valueSoFar);
+            }
+
+            if (result == null || result.equals("")) {
+                return String.valueOf(i);
+            }
+            return result;
+        }
     }
 }
